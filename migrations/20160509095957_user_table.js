fbbaee69ae72b2a665f2f8c701e893ff7d3e9eff
Louisa Reese <louisa.e.reese@gmail.com>
2016-05-10 13:30:43 -0600

    altered migrations to have default score and that username must be unique

--- migrations/20160509095957_user_table.js
+++ migrations/20160509095957_user_table.js
@@ -2,7 +2,7 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('users', function (table) {
     table.increments('id').unique().primary();
-    table.string('username');
+    table.string('username').unique();
     table.string('password');
   });
 };
