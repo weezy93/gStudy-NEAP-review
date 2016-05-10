7f16fbfeee472968fe135b4737a952a481f8aff2
Louisa Reese <louisa.e.reese@gmail.com>
2016-05-10 13:09:35 -0600

    removed console logs

--- src/client/js/app/profile/profile.controller.js
+++ src/client/js/app/profile/profile.controller.js
@@ -7,12 +7,10 @@
   function profileCtrl($stateParams, crudService) {
     var vm = this;
     var userID = $stateParams.userID;
-    console.log('url params', $stateParams);
     vm.allDecks = [];
 
     crudService.getAll('decks/' + userID)
     .then(function (decks) {
-      console.log(decks.data);
       vm.allDecks = decks.data;
     });
   }
